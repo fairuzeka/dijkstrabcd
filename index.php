3ec6305243f25b369aea178c2a3f4c99f73cd10f
iyus <fairuzeka@hotmail.com>
2018-07-02 09:36:29 +0700

    2 Juli 19 - 09:36

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
+<?php
+
+session_start();
+include 'connect.php';
+$error='';
+
+    if (isset($_POST['submit'])) {
+        $nik=$_POST['user'];
+        $pass=$_POST['pass'];    
+        $query = mysqli_query($con,"SELECT*FROM user2 where NIK='$nik' and password='$pass'");
+        $rows = mysqli_num_rows($query);
+        if ($rows==1) {//jika berhasil maka akan bernilai 1
+        $abc=mysqli_fetch_array($query);
+        $levelus=$abc['levelus'];
+        $_SESSION['nik']=$nik;
+        $_SESSION['levelus']=$abc['levelus'];
+        if ($levelus=="pegawai") {
+            header("pegawai.php");
+        }elseif ($levelus=="atasan") {
+            header("atasan.php");
+        }
+        }else{
+             $error="invalid Account";
+        }
+    }
+?>
+
+
+
 
 <!doctype html>
 
@@ -35,23 +64,19 @@
                 cursor: pointer;
                 border-radius: 5px;
                 margin-bottom: 15px;
-            }
-            
+            }          
         </style>
     </head>
     <body>
         <div class="login">
             <h2 align="center">Selamat datang, silahkan login!</h2>
-            <form action="index.php?opq=in" method="POST" style="text-align: center;">          	
+            <form method="POST" style="text-align: center;">          	
                 <input type="text" placeholder="NIK" id="nik" name="user"> </br></br>              
                 <input type="password" placeholder="Password" id="pass" name="pass"> </br></br>
                 <input type="submit" value="Login" name="submit"> 
                 <span>
-                    <? session_start();
-                    echo "$_SESSION['error']"
-                    ?>
+                    <?php echo $error;?>
                 </span>
-
             </form>
         </div>
     </body>
